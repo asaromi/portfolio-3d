36a40fa31ca205fe997b2e8effc0c0c32d260835
Asaromi <a.sabililromi@gmail.com>
2023-10-14 23:23:24 +0700

    fix: add port, host, & rewrite proxy from API_URL

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(
+  ({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '')
+
+    return {
+      plugins: [react()],
+      server: {
+        host: env.VITE_HOST || '0.0.0.0',
+        port: env.VITE_PORT || 8000,
+        proxy: {
+          '/api': {
+            target: env.VITE_API_URL || 'http://localhost:3000',
+            changeOrigin: true,
+            rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+      },
+    }
+  },
+)

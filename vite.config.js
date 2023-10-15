009162fa0d8bf8b3fbf946dbeb5825fd84215e5d
Asaromi <a.sabililromi@gmail.com>
2023-10-15 07:32:47 +0700

    fix: replace sitemap builder with vite-plugin

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig(
@@ -7,7 +8,7 @@ export default defineConfig(
     const env = loadEnv(mode, process.cwd(), '')
 
     return {
-      plugins: [react()],
+      plugins: [react(), Sitemap()],
       server: {
         host: env.VITE_HOST || '0.0.0.0',
         port: env.VITE_PORT || 8000,

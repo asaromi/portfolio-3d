65764933be91dbbc8fcca26ccb45a2004ee6b1d8
Asaromi <a.sabililromi@gmail.com>
2023-11-05 22:27:05 +0700

    fix: add demo_url props

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,59 +10,70 @@ import {fadeIn, textVariant} from '../utils/motion'
 import PropTypes from 'prop-types'
 
 
-const ProjectCard = ({ description, image, index, name, source_code_link, tags }) => (
-  <motion.div
-    variants={fadeIn('up', 'spring', index * 0.5, 0.75)}
-  >
-    <Tilt
-      options={{
-        max: 45,
-        scale:1,
-        speed: 450
-      }}
-      className="bg-tertiary p-5 rounded-2xl sm:w-[300px] w-full"
+const ProjectCard = ({ description, demo_url, image, index, name, source_code_link, tags }) => {
+  const navigateTo = (url) => {
+    window.open(url, '_blank', 'noreferrer')
+  }
+
+  return (
+    <motion.div
+      variants={fadeIn('up', 'spring', index * 0.5, 0.75)}
     >
-      <div className="relative w-full h-[230px]">
-        <img
-          alt={name}
-          src={image}
-          className="w-full h-full object-cover rounded-2xl"
-        />
-
-        <div
-          className="absolute inset-0 flex justify-end m-3 card-img_hover"
+      <Tilt
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary p-5 rounded-2xl sm:w-[300px] w-full"
+      >
+        <button
+          onClick={() => navigateTo(demo_url)}
         >
-          <button
-            className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            onClick={() => window.open(source_code_link, '_blank')}
-          >
+          <div className="relative w-full h-[230px]">
             <img
-              alt="github"
-              src={github}
-              className="w-3/4 h-3/4 object-contain"
+              alt={name}
+              src={image}
+              className="w-full h-full object-cover rounded-2xl"
             />
-          </button>
-        </div>
-      </div>
 
-      <div className="mt-5">
-        <h3 className="text-white font-bold text-[24px]">{name}</h3>
-        <p className="mt-2 text-secondary text-[14px]">{description}</p>
-      </div>
+            <div
+              className="absolute inset-0 flex justify-end m-3 card-img_hover"
+            >
+              <button
+                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                onClick={() => navigateTo(source_code_link)}
+              >
+                <img
+                  alt="github"
+                  src={github}
+                  className="w-3/4 h-3/4 object-contain"
+                />
+              </button>
+            </div>
+          </div>
 
-      <div className="mt-4 flex flex-wrap gap-2">
-        {tags.map((tag) => (
-          <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-            #{tag.name}
-          </p>
-        ))}
-      </div>
-    </Tilt>
-  </motion.div>
-)
+          <div className="mt-5 text-left">
+            <h3 className="text-white font-bold text-[24px]">{name}</h3>
+            <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          </div>
+
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
+        </button>
+      </Tilt>
+    </motion.div>
+  )
+}
 
 ProjectCard.propTypes = {
   description: PropTypes.string,
+  demo_url: PropTypes.string,
   image: PropTypes.string,
   index: PropTypes.number,
   name: PropTypes.string,

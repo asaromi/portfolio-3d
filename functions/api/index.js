d9f4e0b67d0f51a55fabb8e80a80e241b9dc8f43
asaromi <ahmad.maromi@gunungsteel.com>
2024-05-21 17:45:19 +0700

    fix: try cloudflare functions /api with json response

--- functions/api/index.js
+++ functions/api/index.js
@@ -1,7 +1,18 @@
 // define this function in the index.js file
 
 export async function onRequest(context) {
-	console.log(`[LOGGING FROM /hello]: Request came from ${context.request}`);
+	console.log(`[LOGGING FROM /hello]: Request came from ${context.request}`)
+	const response = new Response({
+		body: JSON.stringify({ message: 'Hello from Cloudflare Worker!' }),
+		status: 200,
+		headers: {
+			'Content-Type': 'application/json',
+		},
+	})
 
-	return new Response("Hello, world!");
+	if (!response.ok) {
+		throw new Error(response.statusText)
+	}
+
+	return await response.json()
 }
